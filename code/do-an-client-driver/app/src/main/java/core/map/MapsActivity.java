d0a4b0f77e9df510e434d13970c477081d8fd194
Your Name <you@example.com>
2018-04-20 03:39:22 +0700

    luu

--- code/do-an-client-driver/app/src/main/java/core/map/MapsActivity.java
+++ code/do-an-client-driver/app/src/main/java/core/map/MapsActivity.java
@@ -18,7 +18,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_sau_khi_nhan_quoc);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
